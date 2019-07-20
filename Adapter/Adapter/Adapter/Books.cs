2e8acf25526bca6932216bfdcef73ed7f6f8c84b
xinyutan <xinyutan@umich.edu>
2019-07-20 10:41:21 -0400

    Structural pattern: Adapter

---
+++ Adapter/Adapter/Adapter/Books.cs
@@ -0,0 +1,95 @@
+﻿using System;
+namespace Adapter
+{
+    /// <summary>
+    /// Adaptee class: legacy API
+    /// </summary>
+    class BookDatabase
+    {
+        public double GetStars(string bookName)
+        {
+            switch (bookName)
+            {
+                case "Oblomov":
+                    return 5.0;
+
+                case "The Letters of Vincent van Gogh":
+                    return 5.0;
+
+                case "Design Patterns":
+                    return 3.0;
+
+                default:
+                    return -1.0;
+            }
+        }
+
+        public string GetReview(string bookName)
+        {
+            switch (bookName)
+            {
+                case "Oblomov":
+                    return "This book lets me see myself in a deeper way, as through the Mirror of Erised.";
+
+                case "The Letters of Vincent van Gogh":
+                    return "I felt so sad and touch reading these letters. van Gogh was 'fighting a losing battle', and yet he was so devoted and courageous.";
+
+                case "Design Patterns":
+                    return "I think authors could have made this book much more intuitive and simpler to understand.";
+
+                default:
+                    return "";
+            }
+        }
+    }
+
+    /// <summary>
+    /// Target class
+    /// </summary>
+    public class Book
+    {
+        public string Name { get; set; }
+        public float Stars { get; set; }
+        public string Review { get; set; }
+
+        public Book(string name)
+        {
+            Name = name;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("Stars: {0}", Stars);
+            Console.WriteLine("How do I think of the book?\n{0}", Review);
+        }
+
+        public virtual void LoadData()
+        {
+            Console.WriteLine("\nBook {0}---------------", Name);
+        }
+    }
+
+    /// <summary>
+    /// Adapter class: the wrapper for BookDatabase
+    /// </summary>
+    public class BookDetails : Book
+    {
+        private BookDatabase _bookDatabase;
+
+        public BookDetails(string name):
+            base(name)
+        {
+
+        }
+
+        public override void LoadData()
+        {
+            _bookDatabase = new BookDatabase();
+            Stars = (float)_bookDatabase.GetStars(Name);
+            Review = _bookDatabase.GetReview(Name);
+
+            base.LoadData();
+            ShowInfo();
+        }
+    }
+}
---
+++ Adapter/Adapter/Adapter/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=====Demo for Adapter Pattern=====");
+
+            // non-adpated
+            var unknown = new Book("Design Pattern");
+            unknown.LoadData();
+
+            // adapted
+            var oblomov = new BookDetails("Oblomov");
+            oblomov.LoadData();
+
+            var vanGoghLetter = new BookDetails("The Letters of Vincent van Gogh");
+            vanGoghLetter.LoadData();
+
+            var designPattern = new BookDetails("Design Patterns");
+            designPattern.LoadData();
+
+            _ = Console.ReadKey();
+        }
+    }
+}
