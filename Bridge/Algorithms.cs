b3fdf2fcd61f5bc4ea58541fa848473438caca9c
xinyutan <xinyutan@umich.edu>
2019-07-22 20:48:04 -0400

    structural patter: bridge

--- Bridge/Algorithms.cs
+++ Bridge/Algorithms.cs
@@ -1,10 +1,65 @@
 ﻿using System;
+
+
 namespace Bridge
 {
-    public class Algorithms
+    /// <summary>
+    /// Implementor
+    /// </summary>
+    public interface IAlgorithm
     {
-        public Algorithms()
+        void Run(string name);
+    }
+
+    /// <summary>
+    /// Concrete Implementor A
+    /// </summary>
+    public class LinearRegression : IAlgorithm
+    {
+        public void Run(string name)
         {
+            Console.WriteLine("Running Linear Regression on {0}", name);
+        }
+    }
+
+    /// <summary>
+    /// Concrete Implementor B
+    /// </summary>
+    public class RandomForest: IAlgorithm
+    {
+        public void Run(string name)
+        {
+            Console.WriteLine("Running Random Forest on {0}", name);
+        }
+    }
+
+    /// <summary>
+    /// Abstraction class which has a reference to the IAlgorithm (Implementor)
+    /// </summary>
+    public abstract class Dataset
+    {
+        // reference to Implementer
+        public IAlgorithm _algorithm;
+
+        public abstract void ModelData();
+    }
+
+    /// <summary>
+    /// RefinedAbstraction class 
+    /// </summary>
+    public class TitanicDataset : Dataset
+    {
+        public override void ModelData()
+        {
+            _algorithm.Run("Titanic Dataset");
+        }
+    }
+
+    public class IrisDataset: Dataset
+    {
+        public override void ModelData()
+        {
+            _algorithm.Run("Iris Dataset");
         }
     }
 }
---
+++ Bridge/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Bridge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // client call abstraction
+            var titanic = new TitanicDataset();
+            
+            titanic._algorithm = new LinearRegression();
+            titanic.ModelData();
+
+            Console.WriteLine("Change to another algorithm: ");
+            titanic._algorithm = new RandomForest();
+            titanic.ModelData();
+
+
+            Console.WriteLine("\n=================\n");
+            var iris = new IrisDataset();
+
+            iris._algorithm = new LinearRegression();
+            iris.ModelData();
+
+            Console.WriteLine("Change to another algorithm: ");
+            iris._algorithm = new RandomForest();
+            iris.ModelData();
+
+            _ = Console.ReadKey();
+        }
+    }
+}
