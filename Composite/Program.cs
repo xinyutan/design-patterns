30223bf6b95bdafa827329152cdf4463a389109d
xinyutan <xinyutan@umich.edu>
2019-07-24 19:43:28 -0400

    structural pattern: composite

---
+++ Composite/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Composite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hill = new Auditorium("Hill Auditorium");
+            hill.SubTasks.Add(new ChoralUnion("Beethoven Symphony No.5"));
+
+            var theater = new Theater("Theater Perforance");
+            theater.SubTasks.Add(new Theater("Hamlet"));
+            theater.SubTasks.Add(new RenegadePerformance("The Great Tamer"));
+
+            hill.SubTasks.Add(theater);
+
+            var restaurant = new Restaurant("Zingerman's Bakehouse");
+
+            // the root
+            var trip = new Destination("Ann Arbor");
+            trip.SubTasks.Add(restaurant);
+            trip.SubTasks.Add(hill);
+
+            Console.Write("We are going to visit ");
+            trip.Print();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Composite/Tasks.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Composite
+{
+    /// <summary>
+    /// Component abstract class
+    /// </summary>
+    public abstract class Task
+    {
+        public string Name { get; set; }
+        public List<Task> SubTasks { get; set; }
+
+        public Task(string name)
+        {
+            Name = name;
+            SubTasks = new List<Task>();
+        }
+
+        public void Print(int num = 0)
+        {
+            string printString = "";
+            for(var i=0; i<num; i++)
+            {
+                printString = String.Concat(printString, "====");
+            }
+            printString = String.Concat(printString, " ", this.GetType().Name, ": ", Name);
+
+            Console.WriteLine(printString);
+            foreach(var subtask in SubTasks)
+            {
+
+                subtask.Print(num + 1);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Composite class, the root
+    /// </summary>
+    public class Destination : Task
+    {
+        public Destination(string name) : base(name) { }
+    }
+
+    /// <summary>
+    /// Leaf
+    /// </summary>
+    public class Restaurant : Task
+    {
+        public Restaurant(string name) : base(name) { }
+    }
+
+    /// <summary>
+    /// Composite
+    /// </summary>
+    public class Auditorium : Task
+    {
+        public Auditorium(string name) : base(name) { }
+    }
+
+    /// <summary>
+    ///  Leaf (for Composite Auditorium)
+    /// </summary>
+    public class ChoralUnion : Task
+    {
+        public ChoralUnion(string name): base(name) { }
+    }
+
+    /// <summary>
+    ///  Composite (for Composite Auditorium)
+    /// </summary>
+    public class Theater : Task
+    {
+        public Theater(string name) : base(name) { }
+    }
+
+    public class RenegadePerformance : Task
+    {
+        public RenegadePerformance(string name) : base(name) { }
+    }
+}
