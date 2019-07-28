fe12f7ea92b99ab11d42e54400bcc92faeb173d9
xinyutan <xinyutan@umich.edu>
2019-07-28 13:27:27 -0400

    structural pattern: Decorator

---
+++ Decorator/Book.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace Decorator
+{
+    /// <summary>
+    /// Abstract class for Component
+    /// </summary>
+    abstract class Print
+    {
+        public abstract void Display();
+    }
+
+    /// <summary>
+    /// ConcreteComponent
+    /// </summary>
+    class Newspaper : Print
+    {
+        public string Name { get; set; }
+
+        public override void Display()
+        {
+            Console.WriteLine("{0}: {1}", this.GetType().Name, Name);
+        }
+    }
+
+    /// <summary>
+    /// ConcreteComponent
+    /// </summary>
+    class Book : Print
+    {
+        public string Name { get; set; }
+        public string Author { get; set; }
+
+        public override void Display()
+        {
+            Console.WriteLine("{0}: {1} by {2}", this.GetType().Name, Name, Author);
+        }
+    }
+
+    /// <summary>
+    /// Abstract class for Decorator (which has a reference to component and also comply with component's interface)
+    /// </summary>
+    abstract class Status : Print
+    {
+        protected Print _print;
+
+        public Status (Print print)
+        {
+            _print = print;
+        }
+
+        public override void Display()
+        {
+            _print.Display();
+        }
+    }
+
+    /// <summary>
+    /// Concrete Decorator
+    /// </summary>
+    class Read : Status
+    {
+        public string Review { get; set; }
+
+        public Read(Print print, string review): base(print)
+        {
+            Review = review;
+        }
+
+        public override void Display()
+        {
+            base.Display();
+            Console.WriteLine("I've already read this book. My review is: \n{0}", Review);
+        }
+    }
+
+    /// <summary>
+    /// Concrete Decorator
+    /// </summary>
+    class ToRead: Status
+    { 
+        public string Reason { get; set; }
+
+        public ToRead(Print print, string reason): base(print)
+        {
+            Reason = reason;
+        }
+
+        public override void Display()
+        {
+            base.Display();
+            Console.WriteLine("I want to read this book because: \n{0}", Reason);
+        }
+    }
+
+    /// <summary>
+    /// Concrete Decorator
+    /// </summary>
+    class Subscribe : Status
+    {
+        public Subscribe(Print print) : base(print) { }
+        public override void Display()
+        {
+            Console.WriteLine("I subscribed the following:");
+            base.Display();
+        }
+    }
+}
---
+++ Decorator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Decorator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book1 = new Book() { Author = "Friedrich Nietzsche", Name = "The Birth of Tragedy" };
+            var book2 = new Book() { Author = "Aldous Huxley", Name = "Brave New World" };
+            var newspaper1 = new Newspaper() { Name = "The New York Times" };
+
+            var toRead = new ToRead(book1, "I am interested in knowing more about Nietzsche's philosophy.");
+            toRead.Display();
+
+            Console.WriteLine("==========================");
+            var alreadyRead = new Read(book2, "I really enjoyed this book. The brave new world lets me think about the meaning of life and nature of humanity in this world.");
+            alreadyRead.Display();
+
+            Console.WriteLine("==========================");
+            var subscribe = new Subscribe(newspaper1);
+            subscribe.Display();
+        }
+    }
+}
