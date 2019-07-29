c5703640f5c143563024f9149e0c34b4d4858f55
xinyutan <xinyutan@umich.edu>
2019-07-28 21:40:39 -0400

    structural pattern: Facade

---
+++ Facade/Order.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Facade
+{
+    class Order
+    {
+        string Name { get; set; }
+        int Quantity { get; set; }
+        float UnitPrice { get; set; }
+
+        public Order(string name, int quantity, float unitPrice)
+        {
+            Name = name;
+            Quantity = quantity;
+            UnitPrice = unitPrice;
+        }
+
+        public override string ToString()
+        {
+            return Quantity.ToString() + " " + Name;
+        }
+
+        public float GetAmount()
+        {
+            return UnitPrice * Quantity;
+        }
+    }
+
+    class EmailService
+    {
+        public void SendEmail(string email, DateTime scheduleTime, string content)
+        {
+            Console.WriteLine("Sending an email to {0} at {1} with content '{2}'.", email, scheduleTime.ToString(), content);
+        }
+    }
+
+    class FinancialService
+    {
+        public void Charge(string cardnumber, Order order)
+        {
+            Console.WriteLine("Charging card {0} ${1}.", cardnumber, order.GetAmount());
+        }
+    }
+
+    class ShippingService
+    {
+        public void Ship(Order order, string address)
+        {
+            Console.WriteLine("Shipping {0} to {1}", order.ToString(), address);
+        }
+    }
+
+    /// <summary>
+    /// Facade, the service class that wrapped other process services
+    /// </summary>
+    class OrderService
+    {
+        protected EmailService _emailService;
+        protected FinancialService _financialService;
+        protected ShippingService _shippingService;
+
+        public OrderService()
+        {
+            _emailService = new EmailService();
+            _financialService = new FinancialService();
+            _shippingService = new ShippingService();
+        }
+
+        public void PlaceOrder(Order order, string address, string email, string cardnumber)
+        {
+            _emailService.SendEmail(email, DateTime.Now, "Your order is placed.");
+            _shippingService.Ship(order, address);
+            _financialService.Charge(cardnumber, order);
+        }
+    }
+}
---
+++ Facade/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Facade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var orderService = new OrderService();
+
+            var order1 = new Order("The Fountainhead", 2, (float)7.99);
+            orderService.PlaceOrder(order1, "Ann Arbor, MI", "abc@gmail.com", "1234");
+
+            var order2 = new Order("The Birth of Tragedy", 1, (float)3.99);
+            orderService.PlaceOrder(order2, "Detroit, MI", "xyz@yahoo.com", "56789");
+        }
+    }
+}
