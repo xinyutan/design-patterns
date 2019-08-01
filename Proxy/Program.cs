00c04858db5751c26030d13a92020f6a9b6b0737
xinyutan <xinyutan@umich.edu>
2019-07-31 21:17:22 -0400

    structural pattern: Proxy
    This pattern is used to provide a surrogate or control to another (real)
    object. Here I use E-Reader parental control to demonstrate the
    pattern. Basically our proxy reader can't read children-unfriendly books.

---
+++ Proxy/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Proxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var proxyReader = new ProxyReader();
+
+            var book1 = new Book() { Name = "The Origin of Species by Charles Darwin", IsChildrenFriendly = true };
+            var book2 = new Book() { Name = "Lolita by Vladimir Nabokov", IsChildrenFriendly = false };
+
+            Console.WriteLine("===================\nShould read: ");
+            proxyReader.Read(book1);
+
+            Console.WriteLine("===================\nShould not read: ");
+            proxyReader.Read(book2);
+        }
+    }
+}
---
+++ Proxy/Reader.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Proxy
+{
+    public class Book
+    {
+        public string Name { get; set; }
+        public Boolean IsChildrenFriendly { get; set; }
+    }
+
+    /// <summary>
+    /// The Subject interface that both RealSubject and Proxy will implement
+    /// </summary>
+    public interface IEReader
+    {
+        void Read(Book book);
+    }
+
+    /// <summary>
+    /// RealSubject: Reader for adults
+    /// </summary>
+    class Reader : IEReader
+    {
+        public void Read(Book book)
+        {
+            Console.WriteLine("Reader is reading the book {0}", book.Name);
+        }
+    }
+
+    /// <summary>
+    /// Proxy: control the access to the RealSubject
+    /// </summary>
+    class ProxyReader : IEReader
+    {
+        protected Reader _reader = new Reader();
+        public void Read(Book book)
+        {
+            if (book.IsChildrenFriendly)
+            {
+                _reader.Read(book);
+            }
+            else
+            {
+                Console.WriteLine("ProxyReader can't read the children-unfriendly book {0}", book.Name);
+            }
+        }
+    }
+}
