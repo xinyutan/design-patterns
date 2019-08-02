e93781142d471ca6ac1171bbe1a9acfd89eb8973
xinyutan <xinyutan@umich.edu>
2019-08-01 20:13:43 -0400

    behavioral pattern: Chain of Responsibility
    Chain of Responsility is used to avoid coupling the sender of a request
    to its final receiver by having a middle handler. Here I demonstrate
    this pattern using making a phone call.
    
    For international phone calls, google hangout is much cheaper; but for
    US phone calls, my verizon covers them. So basically whenever I make a
    international phone call using the native app, it transfers the
    responsibility to google hangout.

---
+++ ChainOfResponsibility/PhoneCalls.cs
@@ -0,0 +1,58 @@
+﻿using System;
+namespace ChainOfResponsibility
+{
+    class PhoneNumber
+    {
+        public string Number { get; }
+        public string NationCode { get; }
+
+        public PhoneNumber(string number, string nationCode)
+        {
+            Number = number;
+            NationCode = nationCode;
+        }
+    }
+
+    /// <summary>
+    /// Handler
+    /// </summary>
+    abstract class PhoneApp
+    {
+        protected PhoneApp OtherApp;
+        public abstract void Call(PhoneNumber phoneNumber);
+    }
+
+    /// <summary>
+    /// ConcreteHandler1
+    /// </summary>
+    class GoogleHangout : PhoneApp
+    {
+        public override void Call(PhoneNumber phoneNumber)
+        {
+            Console.WriteLine("Calling phone number {0} from area {1} using Google Hangout.", phoneNumber.Number, phoneNumber.NationCode);
+        }
+    }
+
+    /// <summary>
+    /// ConcreteHandler2: when calling other country's number, transfer the responsibility to GoogleHangout
+    /// </summary>
+    class NativePhone : PhoneApp
+    { 
+        public void SetOtherPhoneApp(PhoneApp phoneApp)
+        {
+            OtherApp = phoneApp;
+        }
+
+        public override void Call(PhoneNumber phoneNumber)
+        {
+            if (phoneNumber.NationCode != "USA")
+            {
+                OtherApp.Call(phoneNumber);
+            }
+            else
+            {
+                Console.WriteLine("Calling phone number {0} from area {1} using Native Phone App.", phoneNumber.Number, phoneNumber.NationCode);
+            }
+        }
+    }
+}
---
+++ ChainOfResponsibility/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ChainOfResponsibility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // client
+            var googleHangoutApp = new GoogleHangout();
+            var nativePhoneApp = new NativePhone();
+            nativePhoneApp.SetOtherPhoneApp(googleHangoutApp);
+
+            var phoneNumber1 = new PhoneNumber("+8613211267511", "China");
+            var phoneNumber2 = new PhoneNumber("+017343851102", "USA");
+
+            Console.WriteLine("Call USA number: ");
+            nativePhoneApp.Call(phoneNumber2);
+
+            Console.WriteLine("\nCall China number using google hangout (cheaper): ");
+            nativePhoneApp.Call(phoneNumber1);
+        }
+    }
+}
