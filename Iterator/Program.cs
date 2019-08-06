6b7d1fef6ecb20e35d0e93a669a322a3603ef786
xinyutan <xinyutan@umich.edu>
2019-08-05 20:54:58 -0400

    behavioral pattern: Iterator

---
+++ Iterator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Iterator
+{
+    public class Client
+    {
+        static void Main()
+        {
+
+            Tree tree = BuildATree();
+            var iterator = tree.CreateIterator();
+
+            foreach (Node node in iterator.Next())
+            {
+                Console.WriteLine("Value: " + node.Val);
+            }
+        }
+
+        public static Tree BuildATree()
+        {
+            Node root = new Node(1);
+            Node node1 = new Node(2);
+            Node node2 = new Node(3);
+            Node node3 = new Node(4);
+            Node node4 = new Node(5);
+            Node node5 = new Node(6);
+            root.Left = node1;
+            root.Right = node2;
+            node1.Left = node3;
+            node1.Right = node4;
+            node2.Right = node5;
+
+            Tree tree = new Tree(root);
+            return tree;
+        }
+    }
+}
---
+++ Iterator/Tree.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Iterator
+{
+    /// <summary>
+    /// Item
+    /// </summary>
+    public class Node
+    {
+        public int Val { get; set; }
+        public Node Right { get; set; }
+        public Node Left { get; set; }
+
+        public Node(int val)
+        {
+            Val = val;
+            Right = null;
+            Left = null;
+        }
+    }
+
+    /// <summary>
+    /// Interface for Aggregate
+    /// </summary>
+    public interface ITree
+    {
+        ITreeIterator CreateIterator();
+    }
+
+    /// <summary>
+    /// Interface for Iterator
+    /// </summary>
+    public interface ITreeIterator
+    {
+        Node First();
+        IEnumerable<Node> Next();
+        bool IsDone();
+    }
+
+    /// <summary>
+    /// ConcreteClass for Aggregate
+    /// </summary>
+    public class Tree : ITree
+    {
+        public Node Head { get; }
+
+        public Tree(Node head)
+        {
+            Head = head;
+        }
+        public ITreeIterator CreateIterator()
+        {
+            return new PreOrderIterator(this);  
+        }
+    }
+
+    /// <summary>
+    /// ConcreteClass for Iterator
+    /// </summary>
+    public class PreOrderIterator : ITreeIterator
+    {
+        private readonly Tree _tree;
+
+        public PreOrderIterator(Tree tree)
+        {
+            _tree = tree;
+        }
+
+        public Node First() => _tree.Head;
+
+        public bool IsDone() => throw new NotImplementedException();
+
+        public IEnumerable<Node> Next()
+        {
+            Stack NodeStack = new Stack();
+            NodeStack.Push(_tree.Head);
+
+            while (NodeStack.Count > 0)
+            {
+                var node = (Node)NodeStack.Pop();
+                yield return node;
+                if (node.Right != null)
+                {
+                    NodeStack.Push(node.Right);
+                }
+                if (node.Left != null)
+                {
+                    NodeStack.Push(node.Left);
+                }
+            }
+        }
+    }
+}
