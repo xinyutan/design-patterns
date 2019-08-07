c1637bcc0655a902a95fd2d869749d31336061b3
xinyutan <xinyutan@umich.edu>
2019-08-06 22:01:41 -0400

    behavioral pattern: Mediator
    Here I use bank and bank accounts to demonstrate the concept. Different
    accounts can't send money to each. They have to go through the
    centralized entity, the bank, in this case. It allows us to have better
    control to the communications between colleagues (bank accounts).

---
+++ Mediator/BankAccounts.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Mediator
+{
+    /// <summary>
+    /// Collegue class
+    /// </summary>
+    public class Account
+    {
+        public string Identifier { get; set; }
+        public double Balance { get; private set; }
+        private readonly IBank _bank;
+
+        public Account(string identifier, double initialBalance, IBank bank)
+        {
+            Identifier = identifier;
+            Balance = initialBalance;
+            _bank = bank;
+        }
+
+        public void Deduct(double amount) => Balance -= amount;
+
+        public void Add(double amount) => Balance += amount;
+
+        public void SendMoney(string emailIdentifier, double amount) => _bank.SendMoney(this, emailIdentifier, amount);
+    }
+
+    /// <summary>
+    /// Interface for mediator
+    /// </summary>
+    public interface IBank
+    {
+        void AddAccount(Account account);
+        void SendMoney(Account fromAccount, string toEmailIdentifier, double amount);
+    }
+
+    /// <summary>
+    /// Concrete mediator between bank accounts
+    /// </summary>
+    public class ABCBank : IBank
+    {
+        public Dictionary<string, Account> Accounts = new Dictionary<string, Account>();
+
+        public void AddAccount(Account account)
+        {
+            Accounts.Add(account.Identifier, account);
+        }
+
+        public void SendMoney(Account fromAccount, string toEmailIdentifier, double amount)
+        {
+            if (!Accounts.ContainsKey(toEmailIdentifier))
+            {
+                throw new Exception(toEmailIdentifier + " is not being registered to an account.");
+            }
+            if (fromAccount.Balance < amount)
+            {
+                throw new Exception("Sending account does not have $" + amount + ".");
+            }
+            Console.WriteLine("Account {0} is sending ${1} to {2}", fromAccount.Identifier, amount, toEmailIdentifier);
+            Console.WriteLine("Before sending the money, from_account has ${0}, to_account has ${1}", fromAccount.Balance, Accounts[toEmailIdentifier].Balance);
+            fromAccount.Deduct(amount);
+            Accounts[toEmailIdentifier].Add(amount);
+            Console.WriteLine("After sending the money, from_account has ${0}, to_account has ${1}", fromAccount.Balance, Accounts[toEmailIdentifier].Balance);
+        }
+    }
+}
---
+++ Mediator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Mediator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ABCBank aBCBank = new ABCBank();
+            Account myAccount = new Account("my@gmail.com", 3000, aBCBank);
+            Account otherAccount = new Account("other@hotmail.com", 4000, aBCBank);
+
+            aBCBank.AddAccount(myAccount);
+            aBCBank.AddAccount(otherAccount);
+
+            myAccount.SendMoney("other@hotmail.com", 1000);
+        }
+    }
+}
