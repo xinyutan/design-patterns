a9ee626e692d51e8175f6a91a17274500e036d75
xinyutan <xinyutan@umich.edu>
2019-08-07 22:06:44 -0400

    behavioral pattern: memento

---
+++ Memento/BookReviews.cs
@@ -0,0 +1,71 @@
+﻿using System;
+namespace Memento
+{
+    public class Review
+    {
+        public string Content;
+        public DateTime LastUpdateTime;
+    }
+
+    /// <summary>
+    /// Originator
+    /// </summary>
+    public class BookReview : Review
+    {
+        public BookReview(string content)
+        {
+            Content = content;
+            LastUpdateTime = DateTime.Now;
+        }
+
+        public BookReviewMemento SaveToMemento()
+        {
+            Console.WriteLine("Saving the current review...\n");
+            return new BookReviewMemento(this);
+        }
+
+        public void Update(string updateContent)
+        {
+            Content = updateContent;
+            LastUpdateTime = DateTime.Now;
+        }
+
+        public void ResetReview()
+        {
+            Content = "";
+            LastUpdateTime = DateTime.Now;
+        }
+
+        public void RetorePreviousReview(BookReviewMemento bookReviewMemento)
+        {
+            Console.WriteLine("Restoring to previous review...\n");
+            Content = bookReviewMemento.Content;
+            LastUpdateTime = bookReviewMemento.LastUpdateTime;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Last Update Time: {0}\nContent: {1}\n", LastUpdateTime.ToString(), Content);
+        }
+    }
+
+    /// <summary>
+    /// Memento
+    /// </summary>
+    public class BookReviewMemento : Review
+    {
+        public BookReviewMemento(BookReview bookReview)
+        {
+            Content = bookReview.Content;
+            LastUpdateTime = bookReview.LastUpdateTime;
+        }
+    }
+
+    /// <summary>
+    /// Caretaker: save and retrieve the memento 
+    /// </summary>
+    class PreviousReview
+    {
+        public BookReviewMemento Memento { get; set; }
+    }
+}
---
+++ Memento/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Memento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BookReview bookReview = new BookReview("I like the book - On the Shortness of Life by Seneca.");
+            bookReview.Display();
+
+            PreviousReview previous = new PreviousReview();
+            previous.Memento = bookReview.SaveToMemento();
+
+            System.Threading.Thread.Sleep(5000);
+            bookReview.Update("I think...ops...I close the file without saving...");
+            // notice 5 seconds delay
+            bookReview.Display();
+
+            bookReview.RetorePreviousReview(previous.Memento);
+            bookReview.Display();
+        }
+    }
+}
