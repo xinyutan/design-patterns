074bee3def0b805e497b3425fad615ca4cc215cd
xinyutan <xinyutan@umich.edu>
2019-08-09 07:57:25 -0400

    behavioral pattern: observer
    I think this pattern might be too old in today's software pattern(?) This
    is that subject push new items to observer. Subject has full control and knowledge about its observers. But in most of today's UI design
    observer subscribes to the subject. The subject is agnostic about its
    observers.

---
+++ Observer/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Observer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Setup all the players
+            Project project = new Project();
+
+            TeamMember teamMember1 = new TeamMember("Jane");
+            TeamMember teamMember2 = new TeamMember("Bob");
+            TeamMember teamMember3 = new TeamMember("Cathy");
+
+            project.AddTeamMember(teamMember1);
+            project.AddTeamMember(teamMember2);
+            project.AddTeamMember(teamMember3);
+
+            Task task1 = new Task
+            {
+                Name = "Research the consumer market",
+                TeamMembers = new HashSet<ITeamMember>() { teamMember1, teamMember2 },
+                IsFinished = false
+            };
+            Console.WriteLine("\nAdding a task");
+            project.AddTask(task1);
+
+            Task task2 = new Task
+            {
+                Name = "Design the software architecture",
+                TeamMembers = new HashSet<ITeamMember>() { teamMember3 },
+                IsFinished = false
+            };
+            Console.WriteLine("\nAdding a task");
+            project.AddTask(task2);
+
+            Console.WriteLine("\nDeleting a task");
+            project.RemoveTask(task1);
+
+            Task task3 = new Task
+            {
+                Name = "Write the initial scaffold of the software",
+                TeamMembers = new HashSet<ITeamMember>() { teamMember3 },
+                IsFinished = false
+            };
+            Console.WriteLine("\nAdding a task");
+            project.AddTask(task3);
+        }
+    }
+}
---
+++ Observer/ProjectManagement.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Observer
+{
+    public class Task
+    {
+        public string Name { get; set; }
+        public HashSet<ITeamMember> TeamMembers { get; set; }
+        public bool IsFinished { get; set; }
+    }
+
+    /// <summary>
+    /// Interface for subject
+    /// </summary>
+    public interface IProject
+    {
+        void Notify(Task task);
+    }
+
+    /// <summary>
+    /// Concrete class for subject
+    /// </summary>
+    public class Project : IProject
+    {
+        private List<ITeamMember> TeamMembers { get; set; }
+        private List<Task> Tasks { get; set; }
+
+        public Project()
+        {
+            TeamMembers = new List<ITeamMember>();
+            Tasks = new List<Task>();
+        }
+
+        public void AddTeamMember(ITeamMember teamMember) => TeamMembers.Add(teamMember);
+
+        public void AddTask(Task task)
+        {
+            if (task.IsFinished)
+            {
+                throw new Exception("Can't add a finished task");
+            }
+            Tasks.Add(task);
+            Notify(task);
+        }
+
+        public void RemoveTask(Task task)
+        {
+            task.IsFinished = true;
+            Tasks.Remove(task);
+            Notify(task);
+        }
+        public void Notify(Task task) => TeamMembers.ForEach(tm => tm.Update(task));
+
+    }
+
+    /// <summary>
+    /// Interface for observer
+    /// </summary>
+    public interface ITeamMember
+    {
+        void Update(Task task);
+    }
+
+    /// <summary>
+    /// Concrete class for observer
+    /// </summary>
+    public class TeamMember : ITeamMember
+    {
+        public string Name { get; set; }
+        private List<Task> Tasks { get; set; }
+        
+        public TeamMember(string name)
+        {
+            Name = name;
+            Tasks = new List<Task>();
+        }
+
+        public void Update(Task task)
+        {
+            if (task.TeamMembers.Contains(this))
+            {
+                Console.WriteLine("Task {0} includes TeamMember {1}", task.Name, Name);
+                Console.WriteLine("Pre-update: {0} tasks", Tasks.Count);
+                if (task.IsFinished)
+                {
+                    Console.WriteLine("Removing this task from this team member.");
+                    Tasks.Remove(task);
+                }
+                else
+                {
+                    Console.WriteLine("Adding this task to this team member.");
+                    Tasks.Add(task);
+                }
+                Console.WriteLine("Post-update: {0} tasks", Tasks.Count);
+            }
+        }
+    }
+}
