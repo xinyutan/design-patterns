2e2e8e253791a7113899d78ec82ab3257f7ecac9
xinyutan <xinyutan@umich.edu>
2019-08-11 20:32:11 -0400

    behavioral pattern: strategy

---
+++ Strategy/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Strategy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TreeNode head = BuildATree();
+            Tree tree = new Tree(head);
+
+            InOrderTraversal inOrderTraversal = new InOrderTraversal();
+            PreOrderTraversal preOrderTraversal = new PreOrderTraversal();
+            PostOrderTraversal postOrderTraversal = new PostOrderTraversal();
+            BFSTraversal bfsTraversal = new BFSTraversal();
+
+            tree.SetTraversalMethod(inOrderTraversal);
+            tree.Print();
+
+            tree.SetTraversalMethod(preOrderTraversal);
+            tree.Print();
+
+            tree.SetTraversalMethod(postOrderTraversal);
+            tree.Print();
+
+            tree.SetTraversalMethod(bfsTraversal);
+            tree.Print();
+        }
+
+        static TreeNode BuildATree()
+        {
+            TreeNode node1 = new TreeNode(1);
+            TreeNode node2 = new TreeNode(2);
+            TreeNode node3 = new TreeNode(3);
+            TreeNode node4 = new TreeNode(4);
+            TreeNode node5 = new TreeNode(5);
+            TreeNode node6 = new TreeNode(6);
+
+            node1.Left = node2;
+            node1.Right = node3;
+
+            node2.Left = node4;
+
+            node3.Left = node5;
+            node3.Right = node6;
+
+            return node1;
+        }
+    }
+}
---
+++ Strategy/TreeTraversal.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Strategy
+{
+    public class TreeNode
+    {
+        public int Val { get; set; }
+        public TreeNode Left { get; set; }
+        public TreeNode Right { get; set; }
+
+        public TreeNode(int val)
+        {
+            Val = val;
+            Left = null;
+            Right = null;
+        }
+    }
+
+    /// <summary>
+    /// Interface for strategy
+    /// </summary>
+    public interface ITraversal
+    {
+        void Traverse(TreeNode node);
+    }
+
+    /// <summary>
+    /// Concrete strategy
+    /// </summary>
+    public class PreOrderTraversal : ITraversal
+    {
+        public void Traverse(TreeNode head)
+        {
+            if (head == null) 
+            {
+                return;
+            }
+            Console.WriteLine("Value: {0}", head.Val);
+            Traverse(head.Left);
+            Traverse(head.Right);
+        }
+    }
+
+    /// <summary>
+    /// Concrete strategy
+    /// </summary>
+    public class InOrderTraversal : ITraversal
+    {
+        public void Traverse(TreeNode head)
+        {
+            if (head == null)
+            {
+                return;
+            }
+            Traverse(head.Left);
+            Console.WriteLine("Value: {0}", head.Val);
+            Traverse(head.Right);
+        }
+    }
+
+    /// <summary>
+    /// Concrete strategy
+    /// </summary>
+    public class PostOrderTraversal : ITraversal
+    {
+        public void Traverse(TreeNode head)
+        {
+            if (head == null)
+            {
+                return;
+            }
+            Traverse(head.Left);
+            Traverse(head.Right);
+            Console.WriteLine("Value: {0}", head.Val);
+        }
+    }
+
+    /// <summary>
+    /// Concrete strategy
+    /// </summary>
+    public class BFSTraversal : ITraversal
+    {
+        public void Traverse(TreeNode head)
+        {
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+            if (head == null)
+            {
+                return;
+            }
+            queue.Enqueue(head);
+            while (queue.Count > 0)
+            {
+                TreeNode node = queue.Dequeue();
+                Console.WriteLine("Value: {0}", node.Val);
+                if (node.Left != null)
+                {
+                    queue.Enqueue(node.Left);
+                }
+                if (node.Right != null)
+                {
+                    queue.Enqueue(node.Right);
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Context
+    /// </summary>
+    public class Tree
+    {
+        private TreeNode _head;
+        private ITraversal _traversal;
+
+        public Tree(TreeNode head)
+        {
+            _head = head;
+        }
+
+        public void SetTraversalMethod(ITraversal traversal)
+        {
+            _traversal = traversal;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("\nTraversing the tree using {0}...", _traversal.GetType().Name);
+            _traversal.Traverse(_head);
+        }
+    }
+}
