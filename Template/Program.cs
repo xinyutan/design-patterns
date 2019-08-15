7682185629e7550ab8d079b847a700c3e4eb59dd
xinyutan <xinyutan@umich.edu>
2019-08-14 20:12:06 -0400

    behavioral pattern: template

---
+++ Template/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Template
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var biology = new CellBiology();
+            biology.Study();
+
+            Console.WriteLine();
+
+            var ml = new MachineLearning();
+            ml.Study();
+        }
+    }
+}
---
+++ Template/Study.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace Template
+{
+    /// <summary>
+    /// abstract class that defines: 1) abstract operations 2) template method defining the skelton of an algorithm
+    /// </summary>
+    public abstract class Subject
+    {
+        public abstract void Read();
+        public abstract void Practice();
+        public abstract void ListenToLecture();
+
+        public void Study()
+        {
+            Console.WriteLine("Studying subject: {0}", this.GetType().Name);
+            ListenToLecture();
+            Read();
+            Practice();
+        }
+    }
+
+    /// <summary>
+    /// concrete class
+    /// </summary>
+    public class MachineLearning : Subject
+    {
+        public override void Read() => Console.WriteLine("Read Pattern Recognition and Machine Learning by Christopher M. Bishop");
+
+        public override void ListenToLecture() => Console.WriteLine("Listen to Machine Learning course by Andrew Ng on Coursera");
+
+        public override void Practice() => Console.WriteLine("Do some toy project on Kaggle");
+
+    }
+
+    /// <summary>
+    /// concrete class
+    /// </summary>
+    public class CellBiology : Subject
+    {
+        public override void Read() => Console.WriteLine("Read Molecular Biology of the Cell by Bruce Alberts et al");
+
+        public override void ListenToLecture() => Console.WriteLine("Listen to open course here: https://ocw.mit.edu/courses/biology/7-012-introduction-to-biology-fall-2004/video-lectures/");
+
+        public override void Practice() => Console.WriteLine("Run some westernblots and PCR at a biology lab");
+    }
+}
