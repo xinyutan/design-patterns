6629f5ea62e1b5fa32eec2628e3e2baf63cefd93
xinyutan <xinyutan@umich.edu>
2019-08-17 12:06:03 -0400

    last pattern: behavioral: visitor

---
+++ Visitor/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Visitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user1 = new YouthUser { Name = "Jane" };
+            User user2 = new YouthUser { Name = "Ron" };
+            User user3 = new AdultUser { Name = "Zoe" };
+            User user4 = new AdultUser { Name = "Wendy" };
+
+            InterestsGroup book = new Book();
+            InterestsGroup music = new Music();
+
+            user1.Accept(book);
+            user1.Accept(music);
+
+            user2.Accept(book);
+
+            user3.Accept(music);
+
+            user4.Accept(music);
+
+            book.Display();
+            Console.WriteLine();
+            music.Display();
+        }
+    }
+}
---
+++ Visitor/UserInterestsGroup.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Visitor
+{
+    /// <summary>
+    /// abstract class for element
+    /// </summary>
+    public abstract class User
+    {
+        public string Name { get; set; }
+        public abstract void Accept(InterestsGroup interestsGroup);
+    }
+
+    /// <summary>
+    /// concrete element class
+    /// </summary>
+    public class YouthUser : User
+    {
+        public override void Accept(InterestsGroup interestsGroup) => interestsGroup.VisitYouthUser(this);
+    }
+
+    /// <summary>
+    /// concrete element class
+    /// </summary>
+    public class AdultUser: User
+    {
+        public override void Accept(InterestsGroup interestsGroup) => interestsGroup.VisitAdultUser(this);
+    }
+
+    /// <summary>
+    /// visitor
+    /// </summary>
+    public abstract class InterestsGroup
+    {
+        protected List<AdultUser> adultUsers = new List<AdultUser>();
+        protected List<YouthUser> youthUsers = new List<YouthUser>();
+
+        public abstract void VisitYouthUser(YouthUser youthUser);
+        public abstract void VisitAdultUser(AdultUser adultUser);
+
+        public void Display()
+        {
+            Console.WriteLine("Youth users who are interested in {0} are: ", this.GetType().Name);
+            youthUsers.ForEach(u => Console.Write(u.Name + "; "));
+            Console.WriteLine("\nAdult users who are interested in {0} are: ", this.GetType().Name);
+            adultUsers.ForEach(u => Console.Write(u.Name + "; "));
+        }
+    }
+
+    public class Book : InterestsGroup
+    {
+        public override void VisitAdultUser(AdultUser adultUser) => adultUsers.Add(adultUser);
+
+        public override void VisitYouthUser(YouthUser youthUser) => youthUsers.Add(youthUser);
+    }
+
+    public class Music : InterestsGroup
+    {
+        public override void VisitAdultUser(AdultUser adultUser) => adultUsers.Add(adultUser);
+
+        public override void VisitYouthUser(YouthUser youthUser) => youthUsers.Add(youthUser);
+
+
+    }
+}
